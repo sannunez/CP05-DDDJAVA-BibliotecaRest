7d90cc668613472e9245093f5168f95122ffa720
unknown <sftwr.gsantosnunes@gmail.com>
2025-10-03 13:46:39 -0300

    Templates e Form

--- biblioteca/src/main/java/com/fiap/biblioteca/controller/HomeController.java
+++ biblioteca/src/main/java/com/fiap/biblioteca/controller/HomeController.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UsuarioController {
-    @GetMapping("/usuarios")
-    public String usuarios() {
-        return "usuarios";
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
     }
+
 }
--- biblioteca/src/main/java/com/fiap/biblioteca/controller/LivroController.java
+++ biblioteca/src/main/java/com/fiap/biblioteca/controller/LivroController.java
@@ -1,15 +1,31 @@
 package com.fiap.biblioteca.controller;
 
+import com.fiap.biblioteca.model.Livro;
+import com.fiap.biblioteca.service.LivroService;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/livros")
 public class LivroController {
+    private final LivroService service;
 
-    @GetMapping("/livros")
+    public LivroController(LivroService service){this.service = service;}
+
+    @GetMapping
     public String livros() {
-        return "livros"; // nome do arquivo .html dentro de templates
+        return "livros";
     }
+
+
+    @GetMapping("/cadastrar")
+    public String  novoProdutoForm(Model model){
+        model.addAttribute("livros", new Livro());
+        return "formCadLivro"
+        ;}
 
 }
 
